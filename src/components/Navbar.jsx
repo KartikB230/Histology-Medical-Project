5ae1f97f8aed0185047b38d1e32df3ef8ef34c63
shubhamnaik2303 <108280093+shubhamnaik2303@users.noreply.github.com>
2024-02-21 18:19:38 +0530

    uu
    uu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 
 <nav class="navbar navbar-expand-lg">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#" style={{'color':'white'}}><strong>SymbiAnatomy.</strong></a>
+    <a class="navbar-brand" href="#" style={{'color':'white', 'padding':'17px 170px'}}><strong>SymbiAnatomy.</strong></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
