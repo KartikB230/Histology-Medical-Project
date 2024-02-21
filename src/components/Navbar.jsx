75f2beaf38c2f10ab4f442e5c0a7553772f1162d
shubhamnaik2303 <108280093+shubhamnaik2303@users.noreply.github.com>
2024-02-21 18:21:44 +0530

    hh
    jh

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 
 <nav class="navbar navbar-expand-lg">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#" style={{'color':'white', 'padding':'17px 170px'}}><strong>SymbiAnatomy.</strong></a>
+    <a class="navbar-brand" href="#" style={{'color':'white', 'padding':'17px 170px', 'fontSize':'28px'}}><strong>SymbiAnatomy.</strong></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -18,21 +18,21 @@ const Navbar = () => {
       <ul class="navbar-nav">
 
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#" style={{'color':'white'}}>Home</a>
+          <a class="nav-link active" aria-current="page" href="#" style={{'color':'white','fontSize':'20px'}}>Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#" style={{'color':'white'}}>Features</a>
+          <a class="nav-link" href="#" style={{'color':'white','fontSize':'20px'}}>Features</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#" style={{'color':'white'}}>Contact</a>
+          <a class="nav-link" href="#" style={{'color':'white','fontSize':'20px'}}>Contact</a>
         </li>
         <li class="nav-item dropdown">
-          <a style={{'color':'white'}} class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a style={{'color':'white','fontSize':'20px'}} class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Learning
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" onClick={() => navigate('SquamousEpithelium')} style={{'color':'rgb(205, 117, 117)', "cursor":"pointer"}}>Squamous Epithelium</a></li>
-            <li><a class="dropdown-item" onClick={() => navigate('Thyroid')} style={{'color':'rgb(205, 117, 117)', "cursor":"pointer"}}>Thyroid</a></li>
+            <li><a class="dropdown-item" onClick={() => navigate('SquamousEpithelium')} style={{'color':'rgb(205, 117, 117)', "cursor":"pointer",'fontSize':'20px'}}>Squamous Epithelium</a></li>
+            <li><a class="dropdown-item" onClick={() => navigate('Thyroid')} style={{'color':'rgb(205, 117, 117)', "cursor":"pointer",'fontSize':'20px'}}>Thyroid</a></li>
             {/* <li><a class="dropdown-item" href="#" style={{'color':'white'}}>Something else here</a></li> */}
           </ul>
         </li>
