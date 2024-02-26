78c671d2f5eeeaf50608cf5c06956d43a61e564c
shashank07100301 <shashank.thakkar.btech2021@sitpune.edu.in>
2024-02-26 13:53:58 +0530

    Squamous

--- src/components/SquamousEpithelium.jsx
+++ src/components/SquamousEpithelium.jsx
@@ -1,16 +1,49 @@
 import React from 'react';
 import '../App.css'; // Import the CSS file
-import { openPopup, closePopup, openModal, closeModal } from './script.js';
 import { useState } from 'react';
+import Navbar from './Navbar.jsx' ;
 
 
 
 
 function SquamousEpithelium() {
-    
+
+  const [buttonClicked, setButtonClicked] = useState(false);
+
+
+  
+  function openPopup1(imagePath, descriptionText, audioPath) 
+  {
+    // Set the source of the pop-up image
+    document.getElementById('popupImage').src = imagePath;
+
+    // Set the source of the pop-up audio
+    document.getElementById('popupAudio').src = audioPath;
+
+    console.log(descriptionText)
+    // Set the description text
+    document.getElementById('popupInfo').innerHTML = descriptionText;
+
+    // Display the overlay
+    document.getElementById('overlay').style.display = 'flex';
+ }
+
+
+ 
+  // Function to close popup
+  function closePopup() 
+  {
+    // Hide the overlay
+    document.getElementById('overlay').style.display = 'none';
+
+    // Pause the audio when closing the pop-up
+    document.getElementById('popupAudio').pause();
+ }
+
       
     return (
       <div>
+        <Navbar/>
         <div className="cd">
           <h1 className="main-title">Squamous Epithelium</h1>
         </div>
@@ -22,22 +55,29 @@ function SquamousEpithelium() {
             src="/assets/simple_squamous_epithelium.jpg"
             className="imgg"
             alt="Simple Squamous Epithelium"
-            onClick={() => openPopup('popup1')}
+            onClick={() => openPopup1("/assets/simple_squamous_epithelium_High_power.jpg", 'Simple squamous epithelium consists of a single layer of flattened cells with a slight bulge in the center because of the presence of the nucleus. Endothelium of blood vessels and alveoli of lungs are lined by simple squamous epithelium', '/assets/Audios/h1_76 squamous epithelium (1).wav')}
             data-popup="popup1"
           />
 
         </div>
+        <strong>
         <p className="image-description">
           Fig 1.1 Simple Squamous Epithelium.{' '}
           <a href="#" style={{ color: 'blue' }}>
             Click on the figure to view High Powered Image.
           </a>
         </p>
+        </strong>
+
+        
 
         {/* Information and audio on the right */}
         <div className="info-container">
           <div className="info-content">
-            <p>
+          <a className="image-cell" onClick={() => openPopup1("/assets/Simple squamous pencil diagram.jpg", 'Simple squamous epithelium consists of a single layer of flattened cells with a slight bulge in the center because of the presence of the nucleus. Endothelium of blood vessels and alveoli of lungs are lined by simple squamous epithelium', '/assets/Audios/h1_76 squamous epithelium (1).wav')}>
+              <strong><u>Click Here to view Pencil Diagram of Simple Squamous Epithelium</u></strong>
+            </a>
+            <p style={{"margin":"10px"}}>
               Simple squamous epithelium consists of a single layer of flattened cells with a slight bulge in the center because of the presence of the nucleus. Endothelium of blood vessels and alveoli of lungs are lined by simple squamous epithelium
             </p>
             <div className="audio-container">
@@ -48,19 +88,35 @@ function SquamousEpithelium() {
                 loop={false}
               />
             </div>
-            <a href="#" className="image-cell">
-              <u>Click Here to view Pencil Diagram of Simple Squamous Epithelium</u>
-            </a>
+            
+          </div>
+        </div>
+
+        <div id="overlay" class="overlay" >
+          <button className="close-button" onClick={() => closePopup('overlay')}>&times;</button>
+          <div className="popup-content">
+            <img id="popupImage" className="popup-image" src="" alt="Pop-up Image" />
+            <div>
+              <p id="popupInfo"></p>
+            </div>
+            <div className="audio-container">
+
+              <audio controls id="popupAudio">
+                <source src="" type="audio/wav" />
+                Your browser does not support the audio element.
+              </audio>
+            </div>
           </div>
         </div>
 
 
         {/* Popup */}
-        <div id="popup1" className="popup">
+        {/* <div id="popup1" className="popup">
           <button className="close-button" data-popup="popup1" onClick={(event) => closePopup(event.target)}>&times;</button>
           <img src="/assets/simple_squamous_epithelium_High_power.jpg" onClick={() => openModal('popup1', '/assets/simple_squamous_epithelium_High_power.jpg', 'Simple Squamous Epithelium Magnified')} height="500px" width="500px" alt="Simple Squamous Epithelium High Power" />
           <div className="description">Follicles are the basic structural units of the thyroid gland. Each lobule contains many closely packed follicles of different sizes and shapes. The size of the follicle is approximately 150-200 microns. Follicles are round to oval in shape and are lined by simple squamous or cuboidal epithelium. The follicles are filled with colloid.</div>
         </div>
+      </div> */}
       </div>
     );
   }
--- src/components/Thyroid.jsx
+++ src/components/Thyroid.jsx
@@ -3,13 +3,12 @@ import '../thyroid.css'; // Import your CSS file
 import Navbar from './Navbar';
 
 function Thyroid() {
- 
+
   const [buttonClicked, setButtonClicked] = useState(false);
 
 
-  
-  function openPopup1(imagePath, descriptionText, audioPath) 
-  {
+
+  function openPopup1(imagePath, descriptionText, audioPath) {
     // Set the source of the pop-up image
     document.getElementById('popupImage').src = imagePath;
 
@@ -22,84 +21,85 @@ function Thyroid() {
 
     // Display the overlay
     document.getElementById('overlay').style.display = 'flex';
- }
+  }
 
 
- 
+
   // Function to close popup
-  function closePopup() 
-  {
+  function closePopup() {
     // Hide the overlay
     document.getElementById('overlay').style.display = 'none';
 
     // Pause the audio when closing the pop-up
     document.getElementById('popupAudio').pause();
- }
+  }
 
 
   // Toggle button visibility
-  function toggleButtons() 
-  {
+  function toggleButtons() {
     const buttonSet = document.getElementById('buttons');
     buttonSet.style.display = buttonSet.style.display === 'none' ? 'block' : 'none';
     setButtonClicked(!buttonClicked); // Toggle the state of buttonClicked
   }
-  
+
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <div className='box'>
-      <div class="heading">
-        <h1>Thyroid</h1><hr/>
-      </div>
-        
+        <div class="heading">
+          <h1>Thyroid</h1><hr />
+        </div>
+
         <div id="labels" className="labels">
 
           <div className="img">
-              <img className="imgg" src="/assets/Updated_thyroid.png" alt="Thyroid" />
-              <button id="toggleButton" data-tooltip = "Show/Hide labels" className="toggle-button" onClick={toggleButtons}>
-                  {buttonClicked ? <img src="/assets/on.png" alt="afterClick" /> : <img src="/assets/off.png" alt="beforeClick" />}
-              </button>
+            <img src="/assets/Updated_thyroid.png" alt="Thyroid" />
+            <button id="toggleButton" data-tooltip="Show/Hide labels" className="toggle-button" onClick={toggleButtons}>
+              {buttonClicked ? <img src="/assets/on.png" alt="afterClick" /> : <img src="/assets/off.png" alt="beforeClick" />}
+            </button>
             <div className='description'>
-                <p>
-                  The thyroid, or thyroid gland, is an endocrine gland in vertebrates. In humans, it is in the neck and consists of two connected lobes. The lower two thirds of 
-                  the lobes are connected by a thin band of tissue called the isthmus. The thyroid gland is a butterfly-shaped gland located in the neck below the Adam's apple. 
-                  Microscopically, the functional unit of the thyroid gland is the spherical thyroid follicle, lined with follicular cells (thyrocytes), and occasional 
-                  parafollicular cells that surround a lumen containing colloid.
-                </p>
+            <a className="image-cell" onClick={() => openPopup1("/assets/Thyroid Pencil diagram without labels.jpg")} style={{"display":"flex","justifyContent":"center","marginBottom":"10px"}}>
+              <strong><u>Click Here to view Pencil Diagram of Simple Squamous Epithelium</u></strong>
+            </a>
+              <p>
+                The thyroid, or thyroid gland, is an endocrine gland in vertebrates. In humans, it is in the neck and consists of two connected lobes. The lower two thirds of
+                the lobes are connected by a thin band of tissue called the isthmus. The thyroid gland is a butterfly-shaped gland located in the neck below the Adam's apple.
+                Microscopically, the functional unit of the thyroid gland is the spherical thyroid follicle, lined with follicular cells (thyrocytes), and occasional
+                parafollicular cells that surround a lumen containing colloid.
+              </p>
             </div>
           </div>
-          <div id = "buttons">
-            <button className="button1" data-tooltip="Follicles" data-popup="popup1" onClick={() => openPopup1('/assets/Connective_Tissue_Septa.png', 'Follicles are the basic structural units of the thyroid gland. Each lobule contains many closely packed follicles of different size and shapes. The size of the follicle is approximately 150-200 microns. Follicles are round to oval in shape and are lined by simple squamous or cuboidal epithelium. The follicles are filled with colloid. ','/assets/Audios/Thyroid_Follicles.wav')}>1</button>
-            <button className="button2" data-tooltip="Connective Tissue septa" data-popup = "popup2" onClick={() => openPopup1('/assets/Connective_Tissue_Septa.png','The gland shows a thin capsule. Thin septae extend from the capsule into the substance of the gland and divide it into lobules. ','/assets/Audios/Connective_tissue_septa.wav')}>2</button>
-            <button className="button3" data-tooltip="Blood Vessels" data-popup = "popup3" onClick={() => openPopup1('#','There is very little intralobular connective tissue (follicles are packed). The interlobular and intralobular connective tissue is thin and contains many capillaries.','#')}>3</button>
-            <button className="button4" data-tooltip="Follicular cells (Simple cuboidal epithelium)" data-popup = "popup4" onClick={() => openPopup1('/assets/Follicular_cells.png','The lining epithelium of the follicles shows cuboidal cells which stain eosinophilic. These cells have a vesicular round nucleus. They constitute the principal secretory cells of thyroid which synthesize thyroglobulin.','/assets/Audios/Follicular_cells.wav' )}>4</button>
-            <button className="button5" data-tooltip="Parafollicular or C cells " data-popup = "popup5" onClick={() => openPopup1('/assets/Parafollicular_Cells.png','These cells constitute about 2% of the thyroid gland. The cells are called as clear cells because the cytoplasm is not stained. The cells are large and occur either singly or in groups of 2 or 3. They are usually situated between the basement membrane and the cubical epithelium. They secrete calcitonin.','/assets/Audios/Parafollicular_cells.wav')}>5</button>
-            <button className="button6" data-tooltip="Colloid" data-popup = "popup6" onClick={() => openPopup1('/assets/Follicular_cells.png','The lumen of the thyroid follicle is large and contains colloid which is stained eosinophilic It is made of a glycoprotein complex, also known as thyroglobulin. The colloid stains with both acidic and basic dyes. It is strongly PAS positive. ','/assets/Audios/Colloid.wav')}>6</button>
+          <div id="buttons">
+            <button className="button1" data-tooltip="Follicles" data-popup="popup1" onClick={() => openPopup1('/assets/Connective_Tissue_Septa.png', 'Follicles are the basic structural units of the thyroid gland. Each lobule contains many closely packed follicles of different size and shapes. The size of the follicle is approximately 150-200 microns. Follicles are round to oval in shape and are lined by simple squamous or cuboidal epithelium. The follicles are filled with colloid. ', '/assets/Audios/Thyroid_Follicles.wav')}>1</button>
+            <button className="button2" data-tooltip="Connective Tissue septa" data-popup="popup2" onClick={() => openPopup1('/assets/Connective_Tissue_Septa.png', 'The gland shows a thin capsule. Thin septae extend from the capsule into the substance of the gland and divide it into lobules. ', '/assets/Audios/Connective_tissue_septa.wav')}>2</button>
+            <button className="button3" data-tooltip="Blood Vessels" data-popup="popup3" onClick={() => openPopup1('#', 'There is very little intralobular connective tissue (follicles are packed). The interlobular and intralobular connective tissue is thin and contains many capillaries.', '#')}>3</button>
+            <button className="button4" data-tooltip="Follicular cells (Simple cuboidal epithelium)" data-popup="popup4" onClick={() => openPopup1('/assets/Follicular_cells.png', 'The lining epithelium of the follicles shows cuboidal cells which stain eosinophilic. These cells have a vesicular round nucleus. They constitute the principal secretory cells of thyroid which synthesize thyroglobulin.', '/assets/Audios/Follicular_cells.wav')}>4</button>
+            <button className="button5" data-tooltip="Parafollicular or C cells " data-popup="popup5" onClick={() => openPopup1('/assets/Parafollicular_Cells.png', 'These cells constitute about 2% of the thyroid gland. The cells are called as clear cells because the cytoplasm is not stained. The cells are large and occur either singly or in groups of 2 or 3. They are usually situated between the basement membrane and the cubical epithelium. They secrete calcitonin.', '/assets/Audios/Parafollicular_cells.wav')}>5</button>
+            <button className="button6" data-tooltip="Colloid" data-popup="popup6" onClick={() => openPopup1('/assets/Follicular_cells.png', 'The lumen of the thyroid follicle is large and contains colloid which is stained eosinophilic It is made of a glycoprotein complex, also known as thyroglobulin. The colloid stains with both acidic and basic dyes. It is strongly PAS positive. ', '/assets/Audios/Colloid.wav')}>6</button>
           </div>
         </div>
-        
-        
+
+
         {/* < Pop-up overlay */}
         <div id="overlay" class="overlay" >
-        <button className="close-button" onClick={() => closePopup('overlay')}>&times;</button>
-        <div className="popup-content">
-            <img id="popupImage" className="popup-image" src="" alt="Pop-up Image"/>
+          <button className="close-button" onClick={() => closePopup('overlay')}>&times;</button>
+          <div className="popup-content">
+            <img id="popupImage" className="popup-image" src="" alt="Pop-up Image" />
             <div>
-            <p id="popupInfo"></p>
+              <p id="popupInfo"></p>
             </div>
             <div className="audio-container">
-            
-                <audio controls id="popupAudio">
-                    <source src="" type="audio/wav"/>
-                    Your browser does not support the audio element.
-                </audio>
+
+              <audio controls id="popupAudio">
+                <source src="" type="audio/wav" />
+                Your browser does not support the audio element.
+              </audio>
             </div>
+          </div>
         </div>
-    </div>
-</div>
-</>
-);
+      </div>
+    </>
+  );
 }
 export default Thyroid;
 
